03bdaa09f16227dd582b18422eaa3645110188c0
Beatriz Gomes <106787519+biagomes07@users.noreply.github.com>
2022-06-30 19:16:57 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake'; 
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = ()=>setToggle(oldToggle => !oldToggle);
+
+useEffect(() => {
+  //Liga flash do celular
+Torch.switchState(toggle);
+},[toggle]);
+
+useEffect(()=> {
+  /**
+   * Quando o celular for chacoalhado, mudaremos o toggle
+   */
+const subscription = RNShake.addListener(() => {
+setToggle(oldToggle => !oldToggle)
+});
+
+//Essa função vai ser chamada quando o componente
+//for desmontado
+return () => subscription.remove();
+}, []);
+
+return ( 
+<View style={toggle ? style.containerLight : style.container}>
+<TouchableOpacity onPress={handleChangeToggle}>
+<Image
+style={toggle ? style.lightingOn : style.lightingOff}
+ source={
+   toggle
+ ? require('./assets/Icons/eco-light.png')
+ : require('./assets/Icons/eco-light-off.png')
+ }
+/>
+<Image
+style={style.dioLogo}
+ source={
+   toggle
+ ? require('./assets/Icons/logo-dio.png')
+ : require('./assets/Icons/logo-dio-white.png')
+ }
+/>
+</TouchableOpacity>
+</View>
+);
+};
+
+export default App;
+
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'black',
+    alignItem: 'center',
+    justifyContent: 'center',
+  },
+
+   containerLight:{
+    flex: 1,
+    backgroundColor: 'white',
+    alignItem: 'center',
+    justifyContent: 'center',
+  },
+
+  lightingOn:{
+    resizeMode:'contain',
+    alignSelf:'center',
+    width: 150,
+    height: 150,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    heigh: 150,
+  },
+  dioLogo: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    heigh: 250,
+  },
+
+});
